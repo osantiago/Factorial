c52abd6f2a91b8d12c9506c52000838fe1e79509
osantiago <orlando.santiago193@yahoo.com>
2019-02-08 20:39:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int i)
+        {
+            if (i <= 1)
+            {
+                return 1;
+            }
+            return i * Factorial(i-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
